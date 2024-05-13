8acc61832e60589ac812f63a09eb19e131ca1850
Anni0p <b.c114303@gmail.com>
2024-05-14 02:26:07 +0530

    PPA

---
+++ JAVA/Command.java
@@ -0,0 +1,16 @@
+class Command 
+{
+    public static void main(String Google[]) 
+    {
+        System.out.println("Number Of CommandLine Arguments  are : "+ Google.length);
+
+        for (int i = 0; i < Google.length; i++)
+        {
+            System.out.println(Google[i]);
+        }
+    }    
+}
+
+
+// javac Command.java
+// java Command 11 PPA ANiket Marvellous LB Marvellous
